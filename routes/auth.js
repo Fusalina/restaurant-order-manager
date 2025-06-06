13a8e84e3d36b557553d86caea2de6e669ea3201
Fusalina <zanotto.carol@gmail.com>
2025-06-05 22:58:29 -0300

    chore: add authentication middleware

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { Customer } = require("../models");
+
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+  const { email } = req.body;
+
+  const user = await Customer.findOne({ where: { email } });
+  if (!user) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign(
+    { id: user.id, email: user.email },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+    }
+  );
+
+  res.json({ token });
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+const authenticateToken = require("../middlewares/authenticateToken");
+
 const customerRoutes = require("./customer");
 const menuRoutes = require("./menu");
 const orderRoutes = require("./order");
+const authRoutes = require("./auth");
 
-// API Versioning
+router.use("/v1/auth", authRoutes);
+
+router.use(authenticateToken);
 router.use("/v1/customer", customerRoutes);
 router.use("/v1/menu", menuRoutes);
 router.use("/v1/order", orderRoutes);
